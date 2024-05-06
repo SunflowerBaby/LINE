af7300abf71616b8880e51384d4a5d21dd07ca9c
vivianchu <vivianchu1123@gmail.com>
2024-05-06 11:29:23 +0800

    LINE NOTIFY 專案

---
+++ WebApplication_LINE/WebApplication_LINE/Controllers/LineMsgController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebApplication_LINE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineMsgController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> ReceiveMessage()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                var body = await reader.ReadToEndAsync();
+                // 在這裡處理收到的訊息，根據您的應用程式需求進行相應的處理
+                // body變數中包含了收到的LINE Notify訊息內容
+                // 例如，您可以解析JSON訊息，並執行相應的業務邏輯
+            }
+
+            return Ok();
+        }
+
+        private object Ok()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication_LINE/WebApplication_LINE/Controllers/LineNotifyController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_LINE.Controllers
+{
+    public class LineNotifyController : Controller
+    {
+
+        // GET: LineNotify
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> SendNotify()
+        {
+            // 請將YOUR_ACCESS_TOKEN替換為您的存取權杖
+            string accessToken = "Gw7zvjqaOdHCiRtzZUuLpxE7zArdfLP8XPNZZ56f4sp";
+
+            // 訊息內容
+            string message = "這是來自ASP.NET Core的LINE Notify訊息！";
+
+            // LINE Notify的API端點
+            string apiEndpoint = "https://notify-api.line.me/api/notify";
+
+            // 使用HttpClient發送POST請求
+            using (HttpClient client = new HttpClient())
+            {
+                // 設置請求標頭
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+
+                // 準備要發送的訊息
+                var content = new StringContent($"message={message}", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+                // 發送POST請求
+                HttpResponseMessage response = await client.PostAsync(apiEndpoint, content);
+
+                // 檢查回應是否成功
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    ViewBag.Message = "訊息已成功發送到LINE Notify！";
+                //}
+                //else
+                //{
+                //    ViewBag.Message = $"發送訊息到LINE Notify失敗，狀態碼：{response.StatusCode}";
+                //}
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Content("訊息已成功發送到LINE Notify！");
+                }
+                else
+                {
+                    return Content($"發送訊息到LINE Notify失敗，狀態碼：{response.StatusCode}");
+                }
+            }
+
+            //return View();
+        }
+    }
+}
